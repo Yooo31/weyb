dc0e8061da03456fc170286d9d10ad2c94c5df5f
Yooo31 <mrts.yoan@gmail.com>
2023-05-30 23:35:11 +0200

    Start of carousel

--- src/assets/js/anim.js
+++ src/assets/js/anim.js
@@ -14,3 +14,26 @@ $(document).ready(function() {
     }
   );
 });
+
+// Sélectionnez les éléments du carrousel
+var carteGauche = document.querySelector('.col-3:nth-child(1) .card');
+var carteCentrale = document.querySelector('.col-6 .card');
+var carteDroite = document.querySelector('.col-3:nth-child(3) .card');
+
+// Ajoutez des écouteurs d'événements de clic aux cartes
+carteGauche.addEventListener('click', function() {
+  echangerContenuCartes(carteCentrale, carteGauche);
+});
+
+carteDroite.addEventListener('click', function() {
+  echangerContenuCartes(carteCentrale, carteDroite);
+});
+
+// Fonction pour échanger le contenu des cartes
+function echangerContenuCartes(carte1, carte2) {
+  var tempHTML = carte1.innerHTML;
+  carte1.innerHTML = carte2.innerHTML;
+  carte2.innerHTML = tempHTML;
+}
+
+
